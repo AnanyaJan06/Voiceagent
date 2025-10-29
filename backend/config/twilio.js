636497d060decbb4c93c4792b04bbfe72d344e5f
Sachin A <sachinpradeepan27@gmail.com>
2025-10-29 15:27:15 +0530

    changes

---
+++ backend/config/twilio.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import twilio from 'twilio';
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+if (!accountSid || !authToken) {
+  throw new Error('Twilio credentials missing in .env');
+}
+
+const client = twilio(accountSid, authToken);
+
+export { client };

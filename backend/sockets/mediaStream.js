9ee790f5e89954e3927283ba6dbdc4d44a303501
Sachin A <sachinpradeepan27@gmail.com>
2025-11-19 16:27:38 +0530

    ssl issue

--- backend/sockets/mediaStream.js
+++ backend/sockets/mediaStream.js
@@ -1,37 +1,37 @@
-import { transcribeAudio } from '../services/stt.js';
-import { getSession, updateSession } from '../utils/state.js';
+// import { transcribeAudio } from '../services/stt.js';
+// import { getSession, updateSession } from '../utils/state.js';
 
-export const setupMediaStream = (io) => {
-  io.on('connection', (socket) => {
-    console.log('Twilio Media Stream connected:', socket.id);
+// export const setupMediaStream = (io) => {
+//   io.on('connection', (socket) => {
+//     console.log('Twilio Media Stream connected:', socket.id);
 
-    socket.on('start', (data) => {
-      console.log('Stream started:', data.streamSid);
-      const session = getSession(data.callSid);
-      // Start session
-    });
+//     socket.on('start', (data) => {
+//       console.log('Stream started:', data.streamSid);
+//       const session = getSession(data.callSid);
+//       // Start session
+//     });
 
-    socket.on('media', async (data) => {
-      const audioBase64 = data.media.payload;
-      const audioBuffer = Buffer.from(audioBase64, 'base64');
+//     socket.on('media', async (data) => {
+//       const audioBase64 = data.media.payload;
+//       const audioBuffer = Buffer.from(audioBase64, 'base64');
       
-      console.log(`Audio chunk: ${audioBuffer.length} bytes`);
+//       console.log(`Audio chunk: ${audioBuffer.length} bytes`);
       
-      // Transcribe with Whisper
-      const text = await transcribeAudio(audioBase64);
-      if (text) {
-        console.log('Whisper transcribed:', text);
+//       // Transcribe with Whisper
+//       const text = await transcribeAudio(audioBase64);
+//       if (text) {
+//         console.log('Whisper transcribed:', text);
         
-        // Update session with transcribed text
-        const session = getSession(data.callSid);
-        // TODO: Process text (next step: Llama)
+//         // Update session with transcribed text
+//         const session = getSession(data.callSid);
+//         // TODO: Process text (next step: Llama)
         
-        // Send text back or trigger response
-      }
-    });
+//         // Send text back or trigger response
+//       }
+//     });
 
-    socket.on('stop', (data) => {
-      console.log('Stream stopped');
-    });
-  });
-};
+//     socket.on('stop', (data) => {
+//       console.log('Stream stopped');
+//     });
+//   });
+// };
